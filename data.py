0907ef09c16d967dde060e05634f5b11032c61ef
ad220 <ad220@users.noreply.github.com>
2025-02-28 00:57:54 +0100

    switch to pycord, add commands, fix data.py

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-DB_PATH = 'data.db'
+DB_PATH = 'data/data.db'
 
 class Database:
     def __init__(self):
@@ -13,15 +13,15 @@ class Database:
 
 
     def create_tables(self) -> None: 
-        self.cur.execute('CREATE TABLE IF NOT EXISTS servers (id INTEGER PRIMARY KEY, name TEXT, xprate_msg INTEGER, xprate_voice INTEGER)')
-        self.cur.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, discord_id INTEGER, server_id INTEGER, xp INTEGER, msg_count INTEGER, voice_uptime INTEGER)') 
+        self.cur.execute('CREATE TABLE IF NOT EXISTS servers (id INTEGER PRIMARY KEY, name TEXT, xprate_msg INTEGER DEFAULT 1, xprate_voice INTEGER DEFAULT 1)')
+        self.cur.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, discord_id INTEGER, server_id INTEGER, xp INTEGER DEFAULT 0, msg_count INTEGER DEFAULT 0, voice_uptime INTEGER DEFAULT 0)') 
         self.cur.execute('CREATE TABLE IF NOT EXISTS roles (id INTEGER PRIMARY KEY, xp_threshold INTEGER, server_id INTEGER)')
         self.cur.execute('CREATE TABLE IF NOT EXISTS channels (id INTEGER PRIMARY KEY, type TEXT, server_id INTEGER)')
         self.con.commit()
 
 
     def add_server(self, server_id: int, server_name: str) -> None:
-        self.cur.execute('INSERT INTO servers VALUES (?, ?, ?, ?)', (server_id, server_name, 0, 0))
+        self.cur.execute('INSERT INTO servers(id, name) VALUES (?, ?)', (server_id, server_name))
         self.con.commit()
 
 
@@ -34,18 +34,23 @@ class Database:
 
 
     def init_users(self, server_id: int, users: list[tuple[str, int]]) -> None:
-        self.cur.executemany('INSERT INTO users VALUES (?, ?, ?, ?)', [(username, discord_id, server_id, 0) for (username, discord_id) in users])
+        self.cur.executemany('INSERT INTO users(username, discord_id, server_id) VALUES (?, ?, ?)', [(username, discord_id, server_id) for (username, discord_id) in users])
         self.con.commit()
 
 
     def add_user(self, server_id: int, username: str, discord_id: int) -> None:
-        self.cur.execute('INSERT INTO users VALUES (?, ?, ?, ?, ?, ?)', (username, discord_id, server_id, 0, 0, 0))
+        self.cur.execute('INSERT INTO users(username, discord_id, server_id) VALUES (?, ?, ?)', (username, discord_id, server_id))
+        self.con.commit()
+
+
+    def rm_user(self, server_id: int, discord_id: int) -> None:
+        self.cur.execute('DELETE FROM users WHERE server_id = ? AND discord_id = ?', (server_id, discord_id))
         self.con.commit()
 
 
     def set_role(self, server_id: int, role_id: int, xp_threshold: int) -> None:
         # check if role already exists
-        self.cur.execute('SELECT * FROM roles WHERE id = ?', (role_id))
+        self.cur.execute('SELECT * FROM roles WHERE id = ?', (role_id,))
         if self.cur.fetchone():
             # update xp_threshold value
             self.cur.execute('UPDATE roles SET xp_threshold = ? WHERE id = ?', (xp_threshold, role_id))
@@ -55,13 +60,13 @@ class Database:
 
 
     def rm_role(self, role_id: int) -> None:
-        self.cur.execute('DELETE FROM roles WHERE id = ?', (role_id))
+        self.cur.execute('DELETE FROM roles WHERE id = ?', (role_id,))
         self.con.commit()
 
 
     def edit_channels(self, server_id: int, channels: list[tuple[int, str]]) -> None:
         self.cur.execute('DELETE FROM channels WHERE server_id = ?', (server_id,))
-        self.cur.executemany('INSERT INTO channels VALUES (?, ?, ?)', [(channel_id, channel_type, server_id, ) for (channel_id, channel_type) in channels])
+        self.cur.executemany('INSERT INTO channels VALUES (?, ?, ?)', [(channel_id, channel_type, server_id) for (channel_id, channel_type) in channels])
         self.con.commit()
 
 
@@ -85,6 +90,16 @@ class Database:
         server_config['channels']['voice'] = self.cur.fetchall()
 
         return server_config
+    
+
+    def set_xp_rate_text(self, server_id: int, xp_rate: int) -> int:
+        self.cur.execute('UPDATE servers SET xprate_msg = ? WHERE id = ?', (xp_rate, server_id))
+        self.con.commit()
+
+    
+    def set_xp_rate_voice(self, server_id: int, xp_rate: int) -> None:
+        self.cur.execute('UPDATE servers SET xprate_voice = ? WHERE id = ?', (xp_rate, server_id))
+        self.con.commit()
 
 
     def get_user_xp(self, server_id: int, discord_id: int) -> int:
@@ -98,7 +113,7 @@ class Database:
 
 
     def get_leaderboard(self, server_id: int) -> list[tuple[str, int]]:
-        self.cur.execute('SELECT username, xp FROM users WHERE server_id = ? ORDER BY xp DESC', (server_id))
+        self.cur.execute('SELECT username, xp FROM users WHERE server_id = ? ORDER BY xp DESC', (server_id,))
         return self.cur.fetchmany(10)
 
 
--- main.py
+++ main.py
@@ -1,29 +1,98 @@
-# This example requires the 'message_content' intent.
-
-import discord
 import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
 import data
 
-token = os.environ['DISCORD_TOKEN']
+GUILD_ID = 1334451132864659500
+
+load_dotenv()
+token = os.environ.get('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = discord.Bot(intents=intents)
 db = data.Database()
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    db.create_tables()
+    print(f'We have logged in as {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.event
+async def on_guild_join(guild):
+    db.add_server(guild.id, guild.name)
+    users = [(member.name, member.id) for member in guild.members]
+    db.init_users(guild.id, users)
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@bot.event
+async def on_guild_remove(guild):
+    db.rm_server(int(guild.id))
 
-client.run(token)
+
+xpbot = bot.create_group('xpbot', "Manage XP Bot", guild_ids=[GUILD_ID])
+
+@xpbot.command(guild_ids=[GUILD_ID])
+async def test(ctx: commands.Context):
+    if ctx.author.guild_permissions.administrator:
+        await ctx.respond('Test command')
+    else:
+        await ctx.respond('You do not have permission to use this command')
+
+@xpbot.command(guild_ids=[GUILD_ID])
+async def get_leaderboard(ctx: commands.Context):
+    users = db.get_leaderboard(ctx.guild.id)
+    await ctx.respond(users)
+
+
+role = xpbot.create_subgroup('role', "Manage automatic roles", guild_ids=[GUILD_ID])
+
+@role.command(guild_ids=[GUILD_ID])
+async def add(ctx: commands.Context, role: discord.Role, xp_threshold: int):
+    if ctx.author.guild_permissions.administrator:
+        db.set_role(ctx.guild.id, role.id, xp_threshold)
+        await ctx.respond('Done')
+    else:
+        await ctx.respond('You do not have permission to use this command')
+
+@role.command(guild_ids=[GUILD_ID])
+async def rm(ctx: commands.Context, role: discord.Role):
+    if ctx.author.guild_permissions.administrator:
+        db.rm_role(role.id)
+        await ctx.respond('Done')
+    else:
+        await ctx.respond('You do not have permission to use this command')
+
+@role.command(guild_ids=[GUILD_ID])
+async def show(ctx: commands.Context):
+    if ctx.author.guild_permissions.administrator:
+        roles = db.get_server_config(ctx.guild.id)['roles']
+        await ctx.respond(roles)
+    else:
+        await ctx.respond('You do not have permission to use this command')
+
+
+xprate = xpbot.create_subgroup('set_xp_rate', "Manage XP rate", guild_ids=[GUILD_ID])
+
+@xprate.command(guild_ids=[GUILD_ID])
+async def voice(ctx: commands.Context, xp_rate: int):
+    if ctx.author.guild_permissions.administrator:
+        db.set_xp_rate_voice(ctx.guild.id, xp_rate)
+        await ctx.respond('Done')
+    else:
+        await ctx.respond('You do not have permission to use this command')
+
+@xprate.command(guild_ids=[GUILD_ID])
+async def text(ctx: commands.Context, xp_rate: int):
+    if ctx.author.guild_permissions.administrator:
+        db.set_xp_rate_text(ctx.guild.id, xp_rate)
+        await ctx.respond('Done')
+    else:
+        await ctx.respond('You do not have permission to use this command')
+
+
+bot.run(token)
 
 
